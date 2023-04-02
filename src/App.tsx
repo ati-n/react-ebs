f74d40671c552dea2deb0a24b9f8501d34b425c8
ati-n <50715221+ati-n@users.noreply.github.com>
2023-04-02 15:59:58 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
                     />
                 </a>
             </div>
-            <h1>This is Vite + React = 💙 </h1>
+            <h1>Vite + React = 💙 </h1>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
